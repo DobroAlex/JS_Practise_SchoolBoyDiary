254f59da1d28e2f5a5fc26bdf1365e4addabb2e5
Alex <alexkoshernosiegov@gmail.com>
2019-06-06 01:19:09 +0300

    Added deleteUser as part of DELETE(CRUD)

--- SchoolBoyDairy/client/src/services/UsersService.js
+++ SchoolBoyDairy/client/src/services/UsersService.js
@@ -4,7 +4,7 @@ export default {
   fetchUsers(){
     return Api().get('users')
   },
-  addUser (params) {
+  addUser(params) {
     return Api().post('users', params)
   },
   updateUser(params) {
@@ -12,6 +12,9 @@ export default {
   },
   getUser(params) {
     return Api().get('user/' + params.id);
+  },
+  deleteUser(id) {
+    return Api().delete('users/' + id)
   }
 
 }
