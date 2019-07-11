25e9a32b7e5952b68b8b6f0f7483140a8de54b1c
Alex <alexkoshernosiegov@gmail.com>
2019-07-11 17:01:50 +0300

    Added 'class' & 'school' params to model

--- SchoolBoyDairy/server_koa/models/user.js
+++ SchoolBoyDairy/server_koa/models/user.js
@@ -3,7 +3,9 @@ const Schema = mongoose.Schema
 
 const UserSchmea = new Schema({
   fullName: String,
-  description: String
+  description: String,
+  school: String,
+  class: String
 })
 
 const User = mongoose.model('User', UserSchmea)
