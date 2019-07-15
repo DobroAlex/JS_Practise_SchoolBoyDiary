70942ec3b2261738cca5b88d40bff3491c8aef7f
Alex <alexkoshernosiegov@gmail.com>
2019-07-15 23:03:04 +0300

    Fixed unused ' ' in json

--- SchoolBoyDairy/server_koa/src/ajv-schems.js
+++ SchoolBoyDairy/server_koa/src/ajv-schems.js
@@ -1,71 +1,71 @@
 module.exports = Object.freeze({
   class_of_school_regex: new RegExp('^\d-[а-я]{1}|university$'), // doesn't work at the moment
   POST_USERS_SCHEMA: {
-    'type': 'object',
-    'properties': {
-      'fullName': {
-        'type': 'string',
-        'minLength': 1
+    type: 'object',
+    properties: {
+      fullName: {
+        type: 'string',
+        minLength: 1
       },
-      'description': {
-        'type': 'string',
-        'minLength': 1
+      description: {
+        type: 'string',
+        minLength: 1
       },
-      'school': {
-        'type': 'string',
-        'minLength': 1
+      school: {
+        type: 'string',
+        minLength: 1
       },
-      'class': {
-        'type': 'string',
-        'pattern': '^\d-[а-я]{1}|university$'
+      class: {
+        type: 'string',
+        pattern: '^\d-[а-я]{1}|university$'
       }
     },
-    'required': ['fullName', 'description', 'school', 'class']
+    required: ['fullName', 'description', 'school', 'class']
   },
   GET_USERS_ID_SCHEMA: { /* https://stackoverflow.com/questions/14940660/whats-mongoose-error-cast-to-objectid-failed-for-value-xxx-at-path-id */
-    'type': 'object',
-    'properties': {
-      'id': {
-        'type': 'string',
-        'pattern': '^[0-9a-fA-F]{24}$|' // checking if :id is even valid
+    type: 'object',
+    properties: {
+      id: {
+        type: 'string',
+        pattern: '^[0-9a-fA-F]{24}$|' // checking if :id is even valid
       }
     },
-    'required': ['id']
+    required: ['id']
   },
   PUT_USERS_ID_SCHEMA: {
-    'type': 'object',
-    'properties': {
-      '_id': {
-        'type': 'string',
-        'pattern': '^[0-9a-fA-F]{24}$'
+    type: 'object',
+    properties: {
+      _id: {
+        type: 'string',
+        pattern: '^[0-9a-fA-F]{24}$'
       },
-      'fullName': {
-        'type': 'string',
-        'minLength': 1
+      fullName: {
+        type: 'string',
+        minLength: 1
       },
-      'description': {
-        'type': 'string',
-        'minLength': 1
+      description: {
+        type: 'string',
+        minLength: 1
       },
-      'school': {
-        'type': 'string',
-        'minLength': 1
+      school: {
+        type: 'string',
+        minLength: 1
       },
-      'class': {
-        'type': 'string',
-        'pattern': '^\d-[а-я]{1}|university$'
+      class: {
+        type: 'string',
+        pattern: '^\d-[а-я]{1}|university$'
       }
     },
-    'required': ['_id', 'fullName', 'description', 'school', 'class']
+    required: ['_id', 'fullName', 'description', 'school', 'class']
   },
   DELETE_USERS_ID_SCHEMA: { /* https://stackoverflow.com/questions/14940660/whats-mongoose-error-cast-to-objectid-failed-for-value-xxx-at-path-id */
-    'type': 'object',
-    'properties': {
-      '_id': {
-        'type': 'string',
-        'pattern': '^[0-9a-fA-F]{24}$' // checking if :id is even valid
+    type: 'object',
+    properties: {
+      _id: {
+        type: 'string',
+        pattern: '^[0-9a-fA-F]{24}$' // checking if :id is even valid
       }
     },
-    'required': ['_id']
+    required: ['_id']
   }
 })
