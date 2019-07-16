9a9bcc1a811cab48d7d452dd50627d496f88a5dc
Alex <alexkoshernosiegov@gmail.com>
2019-07-16 20:19:11 +0300

    Added property for ajv-merge-patch

--- SchoolBoyDairy/server_koa/src/ajv-schems.js
+++ SchoolBoyDairy/server_koa/src/ajv-schems.js
@@ -1,6 +1,7 @@
 module.exports = Object.freeze({
   class_of_school_regex: new RegExp('^\d-[а-я]{1}|university$'), // doesn't work at the moment
   POST_USERS_SCHEMA: {
+    id: 'POST_USERS_SCHEMA',
     type: 'object',
     properties: {
       fullName: {
@@ -23,16 +24,18 @@ module.exports = Object.freeze({
     required: ['fullName', 'description', 'school', 'class']
   },
   GET_USERS_ID_SCHEMA: { /* https://stackoverflow.com/questions/14940660/whats-mongoose-error-cast-to-objectid-failed-for-value-xxx-at-path-id */
+    id: 'GET_USERS_ID_SCHEMA',
     type: 'object',
     properties: {
       id: {
         type: 'string',
-        pattern: '^[0-9a-fA-F]{24}$|' // checking if :id is even valid
+        pattern: '^[0-9a-fA-F]{24}$' // checking if :id is even valid
       }
     },
     required: ['id']
   },
   PUT_USERS_ID_SCHEMA: {
+    id: 'PUT_USERS_ID_SCHEMA',
     type: 'object',
     properties: {
       _id: {
@@ -59,6 +62,7 @@ module.exports = Object.freeze({
     required: ['_id', 'fullName', 'description', 'school', 'class']
   },
   DELETE_USERS_ID_SCHEMA: { /* https://stackoverflow.com/questions/14940660/whats-mongoose-error-cast-to-objectid-failed-for-value-xxx-at-path-id */
+    id: 'DELETE_USERS_ID_SCHEMA',
     type: 'object',
     properties: {
       _id: {
