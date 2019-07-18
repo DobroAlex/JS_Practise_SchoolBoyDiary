079e9c339ae10134053699e41e989030ede655f1
Alex <alexkoshernosiegov@gmail.com>
2019-07-18 15:42:45 +0300

    Fixed according to hints branch

--- SchoolBoyDairy/server_koa/src/validator.js
+++ SchoolBoyDairy/server_koa/src/validator.js
@@ -1,11 +1,9 @@
 module.exports = {
   validate: async function (ajvInstance, schema, data, context, statusCode = 400) {
-    if (!ajvInstance.validate(schema, data)) {
-      context.status = statusCode
-      throw new Error(`${ajvInstance.errorsText()}`)
-    } else {
-      return true
-    }
+    if (ajvInstance.validate(schema, data)) return true
+
+    context.status = statusCode
+    throw new Error(`${ajvInstance.errorsText()}`)
   },
   validateID: async function (Model, id, context) {
     try {
