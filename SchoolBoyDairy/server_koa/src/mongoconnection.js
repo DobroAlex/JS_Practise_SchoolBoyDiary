3eff15de7005b1f86d4703c50c01fd9f3831ebf5
Alex <alexkoshernosiegov@gmail.com>
2019-07-22 23:18:30 +0300

    Fixed 'DeprecationWarning: current URL string parser is deprecated'

--- SchoolBoyDairy/server_koa/src/mongoconnection.js
+++ SchoolBoyDairy/server_koa/src/mongoconnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 module.exports = {
   MONGO_ADDRESS: 'mongodb://localhost:27017/users',
   connectToMongo: function (address) {
-    mongoose.connect(address).then(
+    mongoose.connect(address, { useNewUrlParser: true }).then(
       () => { const db = mongoose.connection; return db }, // on success
       err => { console.log(err); throw new Error('Unable to connect to db') } // this shouldn't happen normally if your mongoDB is online
     )
