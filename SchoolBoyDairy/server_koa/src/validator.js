e37a77d71d8f6fc81975d928ac57b13ed95ff9e0
Alex <alexkoshernosiegov@gmail.com>
2019-07-22 13:54:44 +0300

    Reworked validateID to check empty param, added validateEmail to determine whenever email address exists in db

--- SchoolBoyDairy/server_koa/src/validator.js
+++ SchoolBoyDairy/server_koa/src/validator.js
@@ -1,25 +1,45 @@
 module.exports = {
   validate: async function (ajvInstance, schema, data, context, statusCode = 400) {
-    if (ajvInstance.validate(schema, data)) return true
+    if (ajvInstance.validate(schema, data)) {
+      return true
+    }
 
     context.status = statusCode
     throw new Error(`${ajvInstance.errorsText()}`)
   },
+
   validateID: async function (Model, id, context) {
+    if (id == null) {
+      context = 404
+      throw new Error('ID is undefined. probably wasn\'t created OR already deleted ')
+    }
     try {
-      return !!await Model.findById(id)
+      const res = !!await Model.findById(id)
+      if (!res) {
+        throw new Error()
+      }
     } catch (error) {
-      context.status = 404
       throw new Error(`No such user with id: ${id}`)
     }
   },
-  IsFreeEmail: async function (Model, email, context) {
+
+  ValidateFreeEmail: async function (Model, email, context) {
     const count = await Model.countDocuments({ mail: email })
     if (!count) {
       return true
     } else {
-      context.error = 422
+      context.status = 422
       throw new Error(`E-mail ${email} has already been taken`)
+    }
+  },
+
+  validateEmail: async function (Model, email, context) {
+    const count = await Model.countDocuments({ mail: email })
+    if (count) {
+      return true
+    } else {
+      context.status = 404
+      throw new Error(`No user with ${email} has been found, probably deleted or wasn't created`)
     }
   }
 }
