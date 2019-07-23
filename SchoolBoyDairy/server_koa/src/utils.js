32aed4870f9706deabbe377658c73050cc58afa6
Alex <alexkoshernosiegov@gmail.com>
2019-07-23 18:38:28 +0300

    Added custom error generator with error.status to prevent sending context to non-middleware functions

--- SchoolBoyDairy/server_koa/src/utils.js
+++ SchoolBoyDairy/server_koa/src/utils.js
@@ -2,5 +2,12 @@ module.exports = {
   IsStingNullOrEmpty: async function (str) {
     return (str === null || str === '' || str === undefined)
   },
+
+  errorGenerator: function (msg, status) {
+    const e = new Error(msg)
+    e.status = status
+    return e
+  },
+
   HASH_ROUNDS: 14
 }
