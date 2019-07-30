3718549efc0d2ea0a60a2485af971ea7f6b55977
Alex <alexkoshernosiegov@gmail.com>
2019-07-30 13:32:38 +0300

    Added cors

--- SchoolBoyDairy/server_koa/src/app.js
+++ SchoolBoyDairy/server_koa/src/app.js
@@ -1,6 +1,9 @@
 const koa = require('koa')
 const app = new koa()
 
+const cors = require('@koa/cors')
+app.use(cors())
+
 const bodyParser = require('koa-bodyparser')
 const logger = require('koa-morgan')
 
