41a4d316467be33eb444071c5fa25099454b33e1
Alex <alexkoshernosiegov@gmail.com>
2019-08-04 18:44:21 +0300

    Added methods to perform basic actions with me and refresh token

--- SchoolBoyDairy/client/src/services/UsersService.js
+++ SchoolBoyDairy/client/src/services/UsersService.js
@@ -1,36 +1,52 @@
 import Api from '@/services/Api'
 
 export default {
-  fetchUsers() {
+  fetchUsers () {
     return Api().get('users')
   },
-  addUser(params) {
+  addUser (params) {
     return Api().post('users', params)
   },
-  updateUser(params) {
-    return Api().put('users/' + params.id, params);
+  updateUser (params) {
+    return Api().put('users/' + params.id, params)
   },
-  getUser(params) {
-    return Api().get('user/' + params.id);
+  getUser (params) {
+    return Api().get('user/' + params.id)
   },
-  deleteUser(id) {
-    return Api().delete('users/' + id);
+  deleteUser (id) {
+    return Api().delete('users/' + id)
   },
-  login(email, password) {
-    try{
-      return  Api().post('/public/login', {email: email, password: password})
-    }
-    catch(e){
+  login (email, password) {
+    try {
+      return Api().post('/public/login', {email: email, password: password})
+    } catch (e) {
       throw e
     }
   },
 
-  register(email, password, fullName, school, _class, phoneNumber) {
-    try{
+  register (email, password, fullName, school, _class, phoneNumber) {
+    try {
       return Api().post('/public/register', {email: email, password: password, fullName: fullName, school: school, class: _class, phoneNumber: phoneNumber, description: ' '})
+    } catch (e) {
+      throw e
     }
-    catch(e) {
+  },
+  getMe ({token}) {
+    try {
+      return Api().get('/me', {headers: {
+        authorization: 'Bearer' + token
+      }
+      })
+    } catch (e) {
+      throw e
+    }
+  },
+  refreshMe ({refreshToken}) {
+    try {
+      return Api().post('/me/refresh', {refreshToken: refreshToken})
+    } catch (e) {
       throw e
     }
   }
+
 }
