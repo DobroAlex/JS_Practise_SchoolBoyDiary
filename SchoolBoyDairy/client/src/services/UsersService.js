9e2a1c30a70fb3cf9555363ec66705664e0a76df
Alex <alexkoshernosiegov@gmail.com>
2019-08-04 21:48:24 +0300

    Added put method for /me

--- SchoolBoyDairy/client/src/services/UsersService.js
+++ SchoolBoyDairy/client/src/services/UsersService.js
@@ -47,6 +47,17 @@ export default {
     } catch (e) {
       throw e
     }
+  },
+  putMe(email, fullName, school, _class, phoneNumber, description, token){
+    try {
+      return Api().put('/me', {email: email, fullName: fullName, school: school, class: _class, 
+        phoneNumber: phoneNumber, description: description}, {headers: {
+          authorization: 'Bearer' + token
+        }
+        })
+    }
+    catch(e) {
+      throw e
+    }
   }
-
 }
