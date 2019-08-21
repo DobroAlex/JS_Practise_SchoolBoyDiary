85665a501e8b634d849276e9309e72498f4ae381
Alex <alexkoshernosiegov@gmail.com>
2019-08-21 14:03:36 +0300

    Working on admin page list

--- SchoolBoyDairy/client/src/components/AdminPageList.vue
+++ SchoolBoyDairy/client/src/components/AdminPageList.vue
@@ -1,16 +1,32 @@
 <template>
     <div class="adminPageList">
         <h1>Users</h1>
+        <table>
+            <tr v-for="user in users">
+                <td> {{user._id}} </td>
+                <td> {{user.fullName}} </td>
+                <td> {{user.email}} </td>
+                <td> {{user.description}} </td>
+                <td> {{user.school}} </td>
+                <td> {{user.class}} </td>
+                <td> {{user.phoneNumber}} </td>
+                <td> {{users.role}} </td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script>
-import {getUsers} from '../services/UsersService'
-import { async } from 'q'
+import UsersService from '../services/UsersService'
+import { async, all } from 'q'
+import UsersVue from './Users.vue';
 export default {
     'name': 'AdmiPageList',
     data() {
-        users: []
+        return{
+            users: []
+        }
+
     },
     methods: {
         refreshToken: async function(){
@@ -28,16 +44,15 @@ export default {
         getUsers: async function() {
             try{
                 const response = await UsersService.getUsers(sessionStorage.getItem('token'))
-
                 this.users = response.data.users
             }
-            catch{
+            catch(e) {
                 if (e.response.status == 401) {
-                    await this.refreshToken()
+                    await UsersService.refreshMe({refreshToken: sessionStorage.getItem('refreshToken')})
                     await this.getUsers()
                 }
                 else if (e.response.status == 403) {
-                    alert('Hey, you are not admin, watcha doin\' here')
+                    alert('Hey, you are not admin, watcha doin\' here?')
                     sessionStorage.clear()
                     this.$router.push({name: 'Login'})
                 }
@@ -46,6 +61,15 @@ export default {
                     this.$router.push({name: 'Login'})
                 }
             }
+        }
+    },
+
+    async beforeMount() {
+        try {
+            await this.getUsers()
+        }
+        catch(e) {
+            this.$router.push({name: 'Login'})
         }
     }
 }
