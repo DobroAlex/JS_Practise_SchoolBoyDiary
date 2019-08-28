45a975408f993d202d46e686548d1e671da5b072
Alex <alexkoshernosiegov@gmail.com>
2019-08-28 16:00:03 +0300

    Removed unused components

--- SchoolBoyDairy/client/src/router/index.js
+++ SchoolBoyDairy/client/src/router/index.js
@@ -6,10 +6,6 @@ import VModal  from 'vue-js-modal'
 import Login from '@/components/Login'
 import Registration from '@/components/Registration'
 import Me from '@/components/Me'
-import HelloWorld from '@/components/HelloWorld'
-import Users from '@/components/Users' // this should work. If not, look at replacing / with \\
-import NewUser from '@/components/NewUser'
-import EditUser from '@/components/EditUser'
 import AdminPageList from '@/components/AdminPageList'
 
 Vue.use(Router)
@@ -24,36 +20,19 @@ export default new Router({ //! !!!!https://stackoverflow.com/questions/45992932
       name: 'Login',
       component: Login
     },
+  
     {
       path: '/registration',
       name: 'Registration',
       component: Registration
     },
+  
     {
       path: '/me',
       name: 'Me',
       component: Me
     },
-    {
-      path: '/users',
-      name: 'Users',
-      component: Users
-    },
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/users/new',
-      name: 'NewUser',
-      component: NewUser
-    },
-    {
-      path: '/users/:id',
-      name: 'EditUser',
-      component: EditUser
-    },
+
     {
       path: '/admin/users',
       name: 'AdminPageList',
