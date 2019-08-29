1472009e91ef6f0baeb9aa1ffd9409cd6d91ea60
Alex <alexkoshernosiegov@gmail.com>
2019-08-29 13:21:23 +0300

    implementing logic for elevator which is going up

--- elevatorPractice/client/client/src/classes/elevator.js
+++ elevatorPractice/client/client/src/classes/elevator.js
@@ -1,4 +1,6 @@
 import {elevatorStates} from './elevator_states_enum'
+import {isBetween} from './utils'
+
 module.exports = {
   elevator: class {
     constructor () {
@@ -17,15 +19,17 @@ module.exports = {
       this.state = state
     }
 
-    addFloor (floor, direction) {
+    async addFloor (floor, direction) {
       if (this.floor.includes(floor)) {
         return
       }
       if (this.state === elevatorStates.still) {
-        this.floors.push(floor)
+        await this.floors.push(floor)
       }
-      if (this.state === direction === elevatorStates.goingUp) {  // if elevetor's going up and person wants to go up
-        this.floors.push(floor)  // simply add as a last element
+      if (this.state === direction === elevatorStates.goingUp) { // if elevetor's going up and person wants to go up
+        if (isBetween(this.floors[0], this.floors[1], floor)) {   // and if new floor is between current and target (e.x from 1 to 5 and button pressed at 3 ^)
+          await this.floors.splice(1, 0, floor) // pushing new flor bettwen current and target marking new floor as new target
+        }
       }
     }
   }
