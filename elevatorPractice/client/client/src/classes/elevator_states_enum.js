0ad47c653faa2ca0ba1be37c385f979fb6569dc1
Alex <alexkoshernosiegov@gmail.com>
2019-09-01 18:01:24 +0300

    Moved elevator to separate repo

--- elevatorPractice/client/client/src/classes/elevator_states_enum.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  elevatorStates: Object.freeze({'goingUp': 1, 'goingDown': 2, 'still': 3})
-}
--- elevatorPractice/client/client/src/classes/utils.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  isBetween: function (a, b, c) { // checks if a<=c<=b
-    return (a <= c) && (c <= b)
-  }
-}
