b031cf1524e36601d566a1c06579c78c9d9ae659
Alex <alexkoshernosiegov@gmail.com>
2019-09-06 14:57:17 +0300

    Fixed eslint

--- SchoolBoyDairy/server_koa/database/mongoconnection.js
+++ SchoolBoyDairy/server_koa/database/mongoconnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 module.exports = {
   MONGO_USERS_ADDRESS: 'mongodb://localhost:27017/users',
 
-  connectToMongo: async function ( server, address = this.MONGO_USERS_ADDRESS) {  //TODO: bad OOP
+  connectToMongo: async function (server, address = this.MONGO_USERS_ADDRESS) { // TODO: bad OOP
     try {
       return await mongoose.connect(address, { useNewUrlParser: true })
     } catch (e) {
