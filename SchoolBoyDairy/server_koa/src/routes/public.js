d486da6796e2730113a5a3259836d6cc0c12229c
Alex <alexkoshernosiegov@gmail.com>
2019-09-07 15:43:28 +0300

    Moved public middlewares to separate file

--- SchoolBoyDairy/server_koa/src/routes/public.js
+++ SchoolBoyDairy/server_koa/src/routes/public.js
@@ -1,62 +1,10 @@
+const middleware = require('../middlewares/public')
+
 const Router = require('koa-router')
-const validator = require('../libs/validator')
-const bcrypt = require('bcrypt')
-const ajvUtils = require('../libs/ajv')
-const User = require('../models/user')
-const utils = require('../libs/utils')
-const jwtUtils = require('../libs/jwt-utils')
 const router = new Router()
 
-router.post('/public/register', async (context) => {
-  await validator.validate(ajvUtils.REGISTER_USER_SCHEMA, context.request.body)
+router.post('/public/register', middleware.registerUser)
 
-  const hashedPass = await bcrypt.hash(context.request.body.password, utils.HASH_ROUNDS)
+router.post('/public/login', middleware.postLogin)
 
-  const requestBody = context.request.body
-
-  await validator.ValidateFreeEmail(User, requestBody.email)
-
-  const newUser = new User({
-    fullName: requestBody.fullName,
-    description: requestBody.description,
-    school: requestBody.school,
-    email: requestBody.email,
-    password: hashedPass,
-    class: requestBody.class,
-    phoneNumber: requestBody.phoneNumber,
-    role: 'user' /* by default, each new entity is a simple user. Real admin have to change his role to 'admin'
-                   via MongoDB Compass */
-  })
-  await newUser.save()
-
-  context.ok({ message: `User ${requestBody.fullName}: ${requestBody.email} saved` })
-})
-
-router.post('/public/login', async (context) => {
-  const foundUser = await User.findOne({ email: context.request.body.email }, 'fullName password email role')
-  if (!foundUser) {
-    throw utils.errorGenerator(`No user ${context.request.body.email} has been found`, 404)
-  }
-
-  if (await bcrypt.compare(context.request.body.password, foundUser.password)) {
-    const token = jwtUtils.newAccessToken({
-      email: context.request.body.email,
-      role: foundUser.role
-    })
-    const refreshToken = jwtUtils.newRefreshToken({
-      email: context.request.body.email,
-      role: foundUser.role
-    }, jwtUtils.defaultRefreshExpireTime)
-
-    foundUser.refreshToken = refreshToken
-    await foundUser.save()
-
-    context.send(200, {
-      token: token,
-      refreshToken: refreshToken
-    })
-  } else {
-    throw utils.errorGenerator('Incorrect passwrod', 403)
-  }
-})
 module.exports = router
